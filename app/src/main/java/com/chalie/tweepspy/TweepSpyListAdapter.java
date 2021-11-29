00126d8bddc2d7214e67e2f02293e168b3ec7ea6
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-29 19:43:18 +0300

    create a custom array adapter

---
+++ app/src/main/java/com/chalie/tweepspy/TweepSpyListAdapter.java
@@ -0,0 +1,31 @@
+package com.chalie.tweepspy;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class TweepSpyListAdapter extends ArrayAdapter {
+    private String[] mTweetedCves;
+    private String[] mProduct;
+    private String[] mCveInfo;
+
+    public TweepSpyListAdapter(Context context, int resource, String[] mTweetedCves, String[] mProduct, String[] mCveInfo) {
+        super(context, resource);
+        this.mTweetedCves = mTweetedCves;
+        this.mProduct = mProduct;
+        this.mCveInfo = mCveInfo;
+    }
+
+    @Override
+    public Object getItem(int position){
+        String tweetedCve = mTweetedCves[position];
+        String product = mProduct[position];
+        String cveInfo = mCveInfo[position];
+        return String.format("CVE ID : %s \n -> Affected Vendor : %S", tweetedCve, product);
+    }
+
+
+    @Override
+    public int getCount(){
+        return mTweetedCves.length;
+    }
+}
