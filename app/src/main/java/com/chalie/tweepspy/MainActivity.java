827e35d4729f8e7f56e4cd633456e3e39c46a9c2
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-29 14:40:34 +0300

    bind login button with butterknife

--- app/src/main/java/com/chalie/tweepspy/MainActivity.java
+++ app/src/main/java/com/chalie/tweepspy/MainActivity.java
@@ -3,12 +3,30 @@ package com.chalie.tweepspy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.buttonLogin) Button mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLogin){
+            Toast.makeText(MainActivity.this, "Logging in...", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
